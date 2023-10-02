add021e7e69f49fd6bbbe91efae3b96401dcbf5f
Kasra <kasra.ja1991@gmail.com>
2023-10-02 14:36:06 -0400

    chore: update mpm package

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { getTodaysDate } from 'mpm-packages'
+import { RouterLink, RouterView } from 'vue-router';
+import HelloWorld from './components/HelloWorld.vue';
+import { getTodaysDate } from 'mpm-packages';
 
 
 const today = getTodaysDate();
